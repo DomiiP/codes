09e5ce3c5e883674863119481563a6df1cf8a153
DomiiP <95188800+DomiiP@users.noreply.github.com>
2022-09-05 20:52:04 +0200

    Update max_min_array.cpp
    -Deleted 1 for loop
    -min and max declared at the start
    -A little bit cleaner
    
    Good luck with your programming carrier! :)

--- max_min_array.cpp
+++ max_min_array.cpp
@@ -1,18 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n,i;
+    int n,i,a[n],max=INT16_MIN,min=INT16_MAX;
     cout<<"enter the size of array  :";
     cin>>n;
-    int a[n];
-    int max=0,min=0;
     cout<<"enter array elements"<<endl;
     for(i=0;i<n;i++){
-      cin>>a[i];
-    }
-    min=INT16_MAX;
-    max=INT16_MIN;
-    for(i=0;i<n;i++){
+        cin>>a[i];
         if(a[i]>max){
             max=a[i];
         }
@@ -22,6 +16,4 @@ int main(){
     }
     cout<<"the max number is "<<max<<endl;
     cout<<"the min number is "<<min<<endl;
-
-
 }
